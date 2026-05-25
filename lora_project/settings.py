a2a3e8f7a326fac701495dfe0bfaa524208fd394
Mijailruizg <tu-correo-de-github@example.com>
2026-05-25 15:15:37 -0400

    Simplify SECRET_KEY handling

--- lora_project/settings.py
+++ lora_project/settings.py
@@ -24,12 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-if not SECRET_KEY and os.getenv('RENDER') is None:  # Only use default locally
-    SECRET_KEY = 'django-insecure-$c@dqpatpslru(!v$ahbjk+ni%a_26=n$s%5i6hpi$)lvj)1s8'
-
-if not SECRET_KEY:
-    raise ValueError('SECRET_KEY environment variable is not set')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-$c@dqpatpslru(!v$ahbjk+ni%a_26=n$s%5i6hpi$)lvj)1s8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
